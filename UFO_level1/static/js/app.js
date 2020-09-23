2329310ed74ec71e34a668cd9ccb8991bb194e42
Dave Borowski <dmborowski@msn.com>
2020-09-23 14:31:06 -0600

    Updating code to include all arrays for the data

--- UFO_level1/static/js/app.js
+++ UFO_level1/static/js/app.js
@@ -6,26 +6,36 @@ var Date = [];
 var City = [];
 
 // Iterate through each recipe object
-tableData.forEach((tableData) => {
+// tableData.forEach((tableData) => {
 
-    // Iterate through each key and value
-    Object.entries(tableData).forEach(([key, value]) => {
+//     // Iterate through each key and value
+//     Object.entries(tableData).forEach(([key, value]) => {
   
-      // Use the key to determine which array to push the value to
-      if (key === "datetime") {
-        Date.push(value);
-      }
-      else {
-        City.push(value);
-      }
-     });
-  });
+//       // Use the key to determine which array to push the value to
+//       if (key === "datetime") {
+//         Date.push(value);
+//       }
+//       else {
+//         City.push(value);
+//       }
+//      });
+//   });
 
-  console.log(Date);
+//   console.log(Date);
   
   // Use map to build both arrays of dish and spice values
   var datesMapped = tableData.map(tableData => tableData.datetime);
   var cityMapped = tableData.map(tableData => tableData.city);
+  var stateMapped = tableData.map(tableData => tableData.state);
+  var countryMapped = tableData.map(tableData => tableData.country);
+  var shapeMapped = tableData.map(tableData => tableData.shape);
+  var durationMapped = tableData.map(tableData => tableData.durationMinutes);
+  var commentsMapped = tableData.map(tableData => tableData.comments);
   
   console.log(datesMapped);
   console.log(cityMapped);
+  console.log(stateMapped);
+  console.log(countryMapped);
+  console.log(shapeMapped);
+  console.log(durationMapped);
+  console.log(commentsMapped);
